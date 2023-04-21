ae0e9b7355f0fae66879d4ded156a0830177a5d9
noah.sugarman <noah.sugarman@airbnb.com>
2023-04-21 16:04:34 -0700

    use spinners

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import { execSync } from 'child_process';
+import childProcess, { spawn } from 'child_process';
+import util from 'util';
 import yargs from 'yargs';
 import os from 'os';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
+import ora from 'ora';
+
+const exec = util.promisify(childProcess.exec);
 
 const options = yargs(process.argv.slice(2))
   .command('$0 [rootDir]', `tell Dropbox to ignore node_module files`, (cmd) => {
@@ -14,67 +18,89 @@ const options = yargs(process.argv.slice(2))
   })
   .help().argv;
 
-const rootDir = options.rootDir.replace('~', os.homedir());
+const cwd = options.rootDir.replace('~', os.homedir());
 
-function getNodeModuleDirs() {
-  const files = String(
-    execSync(`rg --files -g '**/node_modules/**' ${rootDir} -u`, {
-      maxBuffer: 1024 * 1024 * 1024, // 1 gig
-    }),
-  )
-    .split('\n')
-    .filter(Boolean);
+async function getNodeModuleDirs(onProgress) {
+  const rg = spawn('rg', ['--files', '-g', '**/node_modules/*', '-u'], {
+    cwd,
+    maxBuffer: 1024 * 1024 * 1024, // 1 gig
+  });
 
-  // "my-project/node_modules/jest/package.json" -> "my-project/node_modules"
-  const nodeModuleDirsWithDupes = files.map((file) => {
+  let data = '';
+  let lineCount = 0;
+  rg.stdout.on('data', (newData) => {
+    data += newData;
+
+    const newLines = (String(newData).match(/\n/g) || '').length;
+    lineCount += newLines;
+    onProgress(lineCount);
+  });
+
+  await new Promise((resolve) => rg.on('close', () => resolve()));
+  const files = data.split('\n').filter(Boolean);
+
+  // convert the full file path to just the node_module directory
+  // e.g. `my-project/node_modules/jest/package.json` -> `my-project/node_modules`
+  const nodeModuleDirs = files.map((file) => {
     return file.replace(/(\b|\/)node_modules(\b|\/).*/, '$1node_modules');
   });
-  return [...new Set(nodeModuleDirsWithDupes)];
+  return [...new Set(nodeModuleDirs)];
 }
 
-function ignorePath(filepath) {
-  execSync(`xattr -w com.dropbox.ignored 1 ${filepath}`);
+async function isAlreadyIgnored(filepath) {
+  const { stdout } = await exec(`xattr -l ${filepath}`, { cwd });
+  return stdout.includes('com.dropbox.ignored: 1');
 }
 
-function isAlreadyIgnored(filepath) {
-  const result = String(execSync(`xattr -l ${filepath}`));
-  return result.includes('com.dropbox.ignored: 1');
+async function ignorePath(filepath) {
+  await exec(`xattr -w com.dropbox.ignored 1 ${filepath}`, { cwd });
 }
 
 (async () => {
-  console.log('Finding node_module directories in', chalk.blue(options.rootDir), '...');
-  const nodeModuleDirs = getNodeModuleDirs();
+  const spinner = ora().start();
 
-  const ignoredDirs = new Set(nodeModuleDirs.filter(isAlreadyIgnored));
-  const dirsToIgnore = nodeModuleDirs.filter((dir) => !ignoredDirs.has(dir));
+  const findingNodeModulesMsg = `Finding node_module directories in ${chalk.blue(options.rootDir)}`;
+  spinner.text = findingNodeModulesMsg;
+  const nodeModuleDirs = await getNodeModuleDirs((numFiles) => {
+    spinner.text = `${findingNodeModulesMsg} (${numFiles} files searched)`;
+  });
 
-  if (ignoredDirs.size > 0) {
-    console.log('Found', ignoredDirs.size, 'already ignored file(s):');
-    for (const dir of ignoredDirs) {
-      console.log(chalk.blue(dir));
+  const dirsToIgnore = [];
+  const alreadyIgnoredDirs = [];
+  for (const nodeModuleDir of nodeModuleDirs) {
+    if (await isAlreadyIgnored(nodeModuleDir)) {
+      alreadyIgnoredDirs.push(nodeModuleDir);
+    } else {
+      dirsToIgnore.push(nodeModuleDir);
+    }
+  }
+
+  if (alreadyIgnoredDirs.length > 0) {
+    spinner.succeed(`Found ${alreadyIgnoredDirs.length} already ignored directories:`);
+    for (const dir of alreadyIgnoredDirs) {
+      console.log(chalk.blue(`   ${dir}`));
     }
   }
 
   if (dirsToIgnore.length > 0) {
-    console.log('\nFound', dirsToIgnore.length, 'file(s) to ignore:');
+    spinner.succeed(`Found ${dirsToIgnore.length} directories to ignore:`);
     for (const dir of dirsToIgnore) {
-      console.log(chalk.blue(dir));
+      console.log(chalk.blue(`   ${dir}`));
     }
 
-    console.log('');
     const { shouldIgnoreFiles } = await inquirer.prompt([
       {
         type: 'confirm',
         name: 'shouldIgnoreFiles',
-        message: `Ignore these ${dirsToIgnore.length} file(s)?`,
+        message: `Ignore ${dirsToIgnore.length} directories?`,
       },
     ]);
-
     if (shouldIgnoreFiles) {
       for (const dir of dirsToIgnore) {
-        ignorePath(dir);
+        await ignorePath(dir);
+        spinner.text = `ignored ${chalk.blue(dir)}`;
       }
-      console.log(`Ignored ${dirsToIgnore.length} file(s).`);
+      spinner.succeed(`Ignored ${dirsToIgnore.length} directories.`);
     }
   }
 })();
